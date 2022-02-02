fcd6ea486d649070efbe6a0645493c5f79168560
Samuel <samlee1097@yahoo.com>
2022-02-02 17:39:06 -0500

    finish tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import io from "socket.io-client";
+import Chat from "./Chat";
+import './App.css'
 
 const socket = io.connect("http://localhost:3001")
 
@@ -7,27 +9,38 @@ function App() {
 
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
+  const [showChat, setShowChat] = useState(false);
 
   function joinRoom(){
       if (username !== "" && room !== ""){
-        
+        socket.emit("join_room", room)
+        setShowChat(true)
       }
   }
 
   return (
     <div className="App">
-      <h3>Join A Chat</h3>
-      <input 
-        type="text" 
-        placeholder="John..." 
-        onChange={e=>setUsername(e.target.value)}
-      />
-      <input 
-        type="text" 
-        placeholder="Room ID..." 
-        onChange={e=>setRoom(e.target.value)}
-      />
-      <button>Join A Room</button>
+      {!showChat ?
+      <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      :
+      <Chat socket={socket} username={username} room={room}/>} 
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -9,20 +9,30 @@ app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+ 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    // socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-
-    socket.on("disconnect", () => {
-        console.log("User Disconnected", socket.id)
-    })
-})
-
 server.listen(3001, () => {
-    console.log("SERVER RUNNING")
+  console.log("SERVER RUNNING");
 });
