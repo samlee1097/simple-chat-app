0da75dc20a22f002a581ee00150ffe47602a7af5
Samuel <samlee1097@yahoo.com>
2022-02-02 16:30:07 -0500

    create new socket.io server

--- server/index.js
+++ server/index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 app.use(cors());
 
 const server = http.createServer(app);
 
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
 server.listen(4000, () => {
     console.log("SERVER RUNNING")
-})
+});
